a391a2d7ffad84145b954fa7e6b2259623813c38
Program Productions <68521982+Programproductions@users.noreply.github.com>
2023-05-31 15:35:23 +1000

    Add new entry to tenant_namespaces

--- tenants.tf
+++ tenants.tf
@@ -1,8 +1,7 @@
-variable "tenant_namespaces" {
-  type = map(object({
+variable "tenant_namespaces" {type = map(object({
     name      = string
     tenant_id = string
-  }))
+"rafico" = {"name":"RAFICO","tenant_id":"rafi789"}}))
   default = {
     "chubb" = { name = "Chubb", tenant_id = "chubb123" },
     "ibm"   = { name = "IBM", tenant_id = "ibm546" }
